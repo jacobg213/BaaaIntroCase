3e2bb2b16915aa4c2e5d5f38bb9a8278717e0e72
Jakub Gilis <gilis.jakub@gmail.com>
2016-08-26 22:24:31 +0200

    Multiple choice means checkboxes!

--- resources/views/quiz.blade.php
+++ resources/views/quiz.blade.php
@@ -14,7 +14,7 @@
 
                         <h3>{{ $question->body }}</h3>
                         @foreach($question->answers as $answer)
-                        {{ Form::radio('answers['. $answer->id .']', $answer->id) }} {{ $answer->body }}
+                        {{ Form::checkbox('answers['. $answer->id .']', $answer->id) }} {{ $answer->body }}
                         @endforeach
 
                         @endforeach
